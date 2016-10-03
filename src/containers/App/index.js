72378bc60e2a33a22369cd260299f71513dd0583
Riku Rouvila <riku.rouvila@gmail.com>
2016-10-03 21:33:10 +0300

    implement initial header

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import cssModules from 'react-css-modules';
+import { login } from 'ducks/user';
+
 import styles from './style.scss';
 
 const App = React.createClass({
   render() {
     return (
       <div styleName="app">
+        <div styleName="header">
+          <div styleName="profile">
+            {
+              this.props.loggedIn ? (
+                <div>
+                  <img styleName="avatar" alt="avatar" src={this.props.profile.picture} />
+                  <span styleName="nickname">{this.props.profile.nickname}</span>
+                </div>
+              ) : (
+                <button styleName="login-link" onClick={this.props.login}>
+                  Login with Github
+                </button>
+              )
+            }
+          </div>
+        </div>
         { this.props.children }
       </div>
     );
   },
 });
 
-function mapStateToProps(state) {
-  return {};
+function mapDispatchToProps(dispatch) {
+  return {
+    login: () => dispatch(login()),
+  };
 }
 
-export default connect(mapStateToProps)(cssModules(App, styles));
+function mapStateToProps(state) {
+  return {
+    loggedIn: state.user.loggedIn,
+    profile: state.user.profile,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(cssModules(App, styles));
